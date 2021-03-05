dc34608315e64e27eaf3722ab88b476afa6964a2
Daniel Knapp <knapparatus@gmail.com>
2021-03-05 17:50:25 -0600

    base html and navbar created, template pages created, routes created for user list, user details, and new user pages

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, render_template,  redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db,  connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+app.config['SECRET_KEY'] = "chickenzarecool21837"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
 connect_db(app)
+
 db.create_all()
+
+@app.route('/')
+def home_page():
+    """Show home Page"""
+
+    return redirect('/users')
+
+@app.route('/users')
+def users_page():
+    """Shows list of all users"""
+    users = User.query.all()
+    return render_template('users.html', users=users)
+
+@app.route('/users/new')
+def create_user_page():
+    """Shows list of all pets in db"""
+    
+    return render_template('create_user.html')
+
+@app.route("/users/<int:user_id>")
+def user_detail_page(user_id):
+    """Show details about a single pet"""
+    user = User.query.get_or_404(user_id)
+    return render_template("user_details.html", user=user)
--- models.py
+++ models.py
@@ -1,1 +1,30 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
 """Models for Blogly."""
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    first_name = db.Column(db.String(50),
+                     nullable=False)
+    
+    last_name = db.Column(db.String(50),
+                     nullable=False)
+    
+    image_url = db.Column(db.String(300),
+                     nullable=False,
+                     default='https://media.istockphoto.com/vectors/no-image-available-sign-vector-id922962354?k=6&m=922962354&s=612x612&w=0&h=_KKNzEwxMkutv-DtQ4f54yA5nc39Ojb_KPvoV__aHyU=')
+
+    def __repr__(self):
+        user = self
+        return f"<User id={user.id} first_name={user.first_name} last_name={user.last_name} image_url={user.image_url}>"
