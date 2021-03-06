977ac6a4654f675b386bf93c122a989af0d6a3b0
Daniel Knapp <knapparatus@gmail.com>
2021-03-06 14:37:57 -0600

    main app functionality complete

--- app.py
+++ app.py
@@ -27,17 +27,67 @@ def home_page():
 @app.route('/users')
 def users_page():
     """Shows list of all users"""
+
     users = User.query.all()
     return render_template('users.html', users=users)
 
 @app.route('/users/new')
 def create_user_page():
-    """Shows list of all pets in db"""
+    """Show form to create new users"""
     
     return render_template('create_user.html')
 
+@app.route('/users/new', methods=["POST"])
+def create_user_db_update():
+    """Add new user to db and redirect to users page"""
+
+    first_name = request.form["first-name"]
+    last_name = request.form["last-name"]
+    image_url = request.form["image-url"]
+    image_url = str(image_url) if image_url else None
+
+    new_user  = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect('/users')
+
 @app.route("/users/<int:user_id>")
 def user_detail_page(user_id):
-    """Show details about a single pet"""
+    """Show user details"""
     user = User.query.get_or_404(user_id)
+
     return render_template("user_details.html", user=user)
+
+@app.route("/users/<int:user_id>/edit")
+def edit_user_page(user_id):
+    """Show form to edit user"""
+    user = User.query.get_or_404(user_id)
+    return render_template("edit_user.html", user=user)
+
+@app.route("/users/<int:user_id>/edit", methods=["POST"])
+def edit_user_db_update(user_id):
+    """Update user in db and redirect to users page"""
+    user = User.query.get_or_404(user_id)
+
+    first_name = request.form["first-name"]
+    last_name = request.form["last-name"]
+    image_url = request.form["image-url"]
+
+    user.first_name = first_name
+    user.last_name = last_name
+    user.image_url = image_url
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect('/users')
+
+@app.route("/users/<int:user_id>/delete", methods=["POST"])
+def delete_user(user_id):
+    """Delete user from db and redirect to users page"""
+    User.query.filter_by(id=user_id).delete()
+
+    db.session.commit()
+
+    return redirect('/users')
